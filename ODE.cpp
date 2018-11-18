7315ee5c40ceb4e2c8e8f05b767b27fdb1356105
Maria Camila Paris <mariacamilaparis@MacBook-de-Juan.local>
2018-11-18 11:14:07 -0500

    2

--- ODE.cpp
+++ ODE.cpp
@@ -10,6 +10,24 @@ double f2y(double t,double x,double y, double dx, double dy, double c, double g,
 double nv(double vy,double vx);
 
 int main(){
+    double g=10.0;
+    double c=0.2;
+    double m=0.2;
+    int pts=45000;
+    double h=0.00001;
+    double t[pts],x[pts],dx[pts],y[pts],dy[pts];
+    double x0=0.0;
+    double y0=0.0;
+    double dx0=150.*sqrt(2);
+    double dy0=150.*sqrt(2);
+    double k1x,k1y,k2x,k2y,k3x,k3y,k4x,k4y,l1x,l1y,l2x,l2y,l3x,l3y,l4x,l4y;
+    for(int i=0;i<pts;i++){
+        t[i]=h*i;
+    }
+    x[0]=x0;
+    dx[0]=dx0;
+    y[0]=y0;
+    dy[0]=dy0;
 }
 
 double f1x(double t,double x,double y, double dx, double dy, double c, double m){
